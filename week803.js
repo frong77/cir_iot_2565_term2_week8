97a47dff29d153f6cea956bb4893eca205ab4aaf
chatmongcol <cdadwngs@gmail.com>
2022-12-08 17:48:51 +0700

    update

---
+++ week803.js
@@ -0,0 +1,13 @@
+//express function
+const hello = function hi(message) {
+    console.log(`Hi! Lung too \n ${message}`)
+}
+
+hello("How are you today!")
+
+const kawna = function tana(message){
+    return `Hi! Lung too \n ${message}`
+}
+
+const demo = kawna("How are you today! ")
+console.log(demo)
---
+++ week804.js
@@ -0,0 +1,11 @@
+//define function
+function sombat(){
+    return `My name is Sombat`
+}
+console.log(sombat())
+
+//Anoymous function
+const  ratapoom = function() {
+    return `My naem is ratapoom`
+}
+console.log(ratapoom())
